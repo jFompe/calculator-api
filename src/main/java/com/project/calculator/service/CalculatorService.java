c8995d668bd0b0662fc8f2c5947a4e4e9f0aff3b
jFompe <jaime.fompe@gmail.com>
2023-05-10 19:01:55 +0200

    Use BinaryOperations in CalculatorService

--- src/main/java/com/project/calculator/service/CalculatorService.java
+++ src/main/java/com/project/calculator/service/CalculatorService.java
@@ -1,6 +1,7 @@
 package com.project.calculator.service;
 
 import com.project.calculator.model.Operation;
+import com.project.calculator.operation.BinaryOperations;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,8 +9,8 @@ public class CalculatorService {
 
     public Integer performBinaryOperation(Integer a, Operation op, Integer b) {
         return switch (op) {
-            case PLUS -> a + b;
-            case MINUS -> a - b;
+            case PLUS -> BinaryOperations.sum(a, b);
+            case MINUS -> BinaryOperations.substract(a, b);
         };
     }
 
