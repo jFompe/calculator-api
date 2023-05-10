934b8f11f986f5c3ad1430d5c86104d0ba7c90c3
jFompe <jaime.fompe@gmail.com>
2023-05-10 19:01:23 +0200

    Define static binary operations

---
+++ src/main/java/com/project/calculator/operation/BinaryOperations.java
@@ -0,0 +1,13 @@
+package com.project.calculator.operation;
+
+public class BinaryOperations {
+
+    public static Integer sum(Integer a, Integer b) {
+        return a + b;
+    }
+
+    public static Integer substract(Integer a, Integer b) {
+        return a - b;
+    }
+
+}
